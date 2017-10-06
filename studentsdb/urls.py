d4a8501b814339919136bf794d85373dc090f311
svartmetall <grongad@gmail.com>
2017-10-06 19:53:12 +0300

    students_list connected to app.

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+
+from students import views
+
+
+urlpatterns = [url(r'^$', views.students_list, name='home'), url(r'^admin/', include(admin.site.urls))]
